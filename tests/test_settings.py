a899a9dd6e018db58ec47e62af818fa13890b1a0
Elijah-Ajadi <ajadiademola926@gmail.com>
2026-05-06 13:17:17 +0100

    fix

--- tests/test_settings.py
+++ tests/test_settings.py
@@ -51,11 +51,13 @@ TEMPLATES = [
     },
 ]
 
+import dj_database_url
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.config(
+        default=f'sqlite:///{os.path.join(BASE_DIR, "db.sqlite3")}',
+        conn_max_age=600
+    )
 }
 
 AUTHENTICATION_BACKENDS = [
